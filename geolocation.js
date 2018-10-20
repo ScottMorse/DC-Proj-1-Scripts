daefbb042cfdb1366330b58d8425eebc05a64087
Scott Morse <scottmorsedev@gmail.com>
2018-10-20 17:40:02 -0500

    Small fix

--- geolocation.js
+++ geolocation.js
@@ -7,25 +7,19 @@ const geoOptions = {
     timeout           : 300000,
 }
 
-navigator.geolocation.getCurrentPosition(geoSuccess,geoError,geoOptions)
-
 let watchID
 if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(geoSuccess,geoError,geoOptions)
     watchID = navigator.geolocation.watchPosition(geoSuccess,geoError,geoOptions)
 } 
 else {
     console.log('Geolocation is not available on this browser.')
 }
 
-let mapImgSrc
 function geoSuccess(position){
     userLat = position.coords.latitude
     userLon =  position.coords.longitude
     console.log(position.coords.latitude, position.coords.longitude)
-    // mapImgSrc = "https://maps.googleapis.com/maps/api/staticmap?center=" + userLat + "," + userLon + "&zoom=13&size=300x300&sensor=false"
-    // const newImg = document.createElement('img')
-    // newImg.src = mapImgSrc
-    // document.body.appendChild(newImg)
 }
 
 function geoError(){
