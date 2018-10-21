40250a287d2d68eb32d48a0bab522119440989a7
Scott Morse <scottmorsedev@gmail.com>
2018-10-20 19:24:25 -0500

    Added Google maps script.

---
+++ googlemaps.js
@@ -0,0 +1,17 @@
+let map
+let marker
+function initMap() {
+    const userLoc = {lat: userLat, lng: userLon};
+    map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 14, center: userLoc});
+    marker = new google.maps.Marker({position: userLoc, map: map});
+}
+
+function updateMap(latitude,longitude){
+    const pos = {lat: latitude, lng: longitude}
+    map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 14, center: pos});
+    marker = new google.maps.Marker({position: pos, map: map});
+}
+
+setTimeout(updateMap,5000)
